15c70b20e2e5a089e57c000139cba03ed46238f9
David Anderson <matandersonlv@hotmail.com>
2018-09-20 17:26:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,16 @@
 # Creating a new class called "Dog"
 class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
 
 end
