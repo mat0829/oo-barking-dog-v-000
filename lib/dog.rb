4eb931cb4e73daeed1207e1f9fb2f85d108a5b17
David Anderson <matandersonlv@hotmail.com>
2018-09-20 17:41:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 # Creating a new class called "Dog"
-class Person
- 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+class Dog
+
+  def initialize(name, bark)
+    @name = name
+    @bark = bark
   end
- 
+
   def name=(full_name)
     first_name, last_name = full_name.split
     @first_name = first_name
     @last_name = last_name
   end
- 
+
   def name
     "#{@first_name} #{@last_name}".strip
   end
- 
+
 end
