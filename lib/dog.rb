2e88308faa9be62f576141cd64a433dc66433636
David Anderson <matandersonlv@hotmail.com>
2018-09-20 17:47:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,15 @@
 # Creating a new class called "Dog"
 class Dog
-
-  class Person
- 
   def initialize(name)
     @name = name
   end
- 
+
   def name
     @name
   end
- 
+
   def name=(new_name)
     @name = new_name
   end
- 
-end
 
+end
